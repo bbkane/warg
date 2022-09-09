7107f805aa0b76e33b722b8f0dad944ff93a3640
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2022-09-08 20:22:33 -0700

    basic nowrap help

--- help/columns/columns.go
+++ help/columns/columns.go
@@ -1,3 +1,78 @@
 package columns
 
-// func CommandHelp()
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"go.bbkane.com/warg/command"
+	"go.bbkane.com/warg/flag"
+	"go.bbkane.com/warg/help/common"
+)
+
+const flagHelpSep = " : "
+const flagIndent = "  "
+const flagAliasNameSep = ", "
+
+// padding returns the spaces to pad a name to fit a given length
+func padding(n flag.Name, length int) string {
+	lenFlagName := len(n)
+	if lenFlagName > length {
+		panic("lenFlagName > length")
+	}
+	return strings.Repeat(" ", length-lenFlagName)
+}
+
+// maxFlagColWidth calculates the max width of everything in the flag column.
+// NOTE: flagIndent is just considered it's own column
+func maxFlagColWidth(fm flag.FlagMap) int {
+	m := 0
+	for name, fl := range fm {
+		lenNameAliasOptionalSep := len(name)
+		if fl.Alias != "" {
+			lenNameAliasOptionalSep = lenNameAliasOptionalSep + len(fl.Alias) + len(flagAliasNameSep)
+		}
+		if lenNameAliasOptionalSep > m {
+			m = lenNameAliasOptionalSep
+		}
+	}
+	return m
+}
+
+func printNoSpace(s ...any) {
+	for _, si := range s {
+		fmt.Print(si)
+	}
+}
+
+func printlnNoSpace(s ...any) {
+	printNoSpace(s...)
+	fmt.Println()
+}
+
+func NoWrapCommandHelp(file *os.File, cur *command.Command, hi common.HelpInfo) command.Action {
+	return func(ctx command.Context) error {
+		maxFlagColWidth_ := maxFlagColWidth(hi.AvailableFlags)
+		// fmt.Println(flagIndent + strings.Repeat("_", maxFlagColWidth_))
+		for _, flagName := range hi.AvailableFlags.SortedNames() {
+			fl := hi.AvailableFlags[flagName]
+			fmt.Print(flagIndent)
+			paddingWidth := maxFlagColWidth_
+			// Adjust padding if we get an alias
+			if string(fl.Alias) != "" {
+				printNoSpace(
+					string(fl.Alias),
+					flagAliasNameSep,
+				)
+				paddingWidth = paddingWidth - len(fl.Alias) - len(flagAliasNameSep)
+			}
+			printlnNoSpace(
+				string(flagName),
+				padding(flagName, paddingWidth),
+				flagHelpSep,
+				string(fl.HelpShort),
+			)
+		}
+		return nil
+	}
+}
--- help/help.go
+++ help/help.go
@@ -5,6 +5,7 @@ import (
 
 	"go.bbkane.com/warg/command"
 	"go.bbkane.com/warg/help/allcommands"
+	"go.bbkane.com/warg/help/columns"
 	"go.bbkane.com/warg/help/common"
 	"go.bbkane.com/warg/help/detailed"
 	"go.bbkane.com/warg/section"
@@ -24,10 +25,30 @@ type HelpFlagMapping struct {
 // Feel free to use it as a base to custimize help functions for your users
 func BuiltinHelpFlagMappings() []HelpFlagMapping {
 	return []HelpFlagMapping{
-		{Name: "default", CommandHelp: detailed.DetailedCommandHelp, SectionHelp: allcommands.AllCommandsSectionHelp},
-		{Name: "detailed", CommandHelp: detailed.DetailedCommandHelp, SectionHelp: detailed.DetailedSectionHelp},
-		{Name: "outline", CommandHelp: OutlineCommandHelp, SectionHelp: OutlineSectionHelp},
+		{
+			Name:        "default",
+			CommandHelp: detailed.DetailedCommandHelp,
+			SectionHelp: allcommands.AllCommandsSectionHelp,
+		},
+		{Name: "detailed",
+			CommandHelp: detailed.DetailedCommandHelp,
+			SectionHelp: detailed.DetailedSectionHelp,
+		},
+
+		{Name: "outline",
+			CommandHelp: OutlineCommandHelp,
+			SectionHelp: OutlineSectionHelp,
+		},
+
 		// allcommands list child commands, so it doesn't really make sense for a command
-		{Name: "allcommands", CommandHelp: detailed.DetailedCommandHelp, SectionHelp: allcommands.AllCommandsSectionHelp},
+		{Name: "allcommands",
+			CommandHelp: detailed.DetailedCommandHelp,
+			SectionHelp: allcommands.AllCommandsSectionHelp,
+		},
+
+		{Name: "nowrap",
+			CommandHelp: columns.NoWrapCommandHelp,
+			SectionHelp: allcommands.AllCommandsSectionHelp,
+		},
 	}
 }
